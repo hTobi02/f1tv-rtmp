ed9dbfd72a2a8986b6896a7efda4d5fe1bd505fc
SeniorHeld <67925751+SeniorHeld@users.noreply.github.com>
2021-12-09 11:59:13 +0100

    remove desync audio/video

--- README.md
+++ README.md
@@ -12,9 +12,9 @@
 
 `docker run --rm -it -p 1935:1935 -e F1TV_EMAIL="<YOUR F1TV EMAIL>" -e F1TV_PASSWORD="<YOUR F1TV PASSWORD>" htobi02/f1tv:<image-tag>`
 
-~~If your want to record every Session use:~~ \
-~~`docker run --rm -it -p 1935:1935 -e F1TV_EMAIL="<YOUR F1TV EMAIL>" -e F1TV_PASSWORD="<YOUR F1TV PASSWORD>" -v /host/path/for/saving/sessions/:/record -e RECORD="$true" htobi02/f1tv`~~
-
 ## ToDo
 [x] Add other languages (german and english) \
+[x] Remove audio/video desync \
+[ ] Add HLS and/or DASH \
+[ ] Use Alpine Image \
 [ ] Add Recording Feature
--- app/f1tv-last.sh
+++ app/f1tv-last.sh
@@ -19,11 +19,11 @@ while $true; do
   else
     #if [[ -v $RECORD ]]; then
       #Play session with streamlink and send it with ffmpeg to a rtmp stream server
-      streamlink $URL best --hls-audio-select="de" --hls-duration 02:30:00 --record-and-pipe /dev/null | ffmpeg -hide_banner -loglevel quiet -stats -re -i pipe: -c:v copy -c:a aac -f flv rtmp://127.0.0.1:1935/live/f1tv
+      streamlink $URL best --hls-audio-select="de" --hls-duration 02:30:00 --record-and-pipe /dev/null | ffmpeg -hide_banner -loglevel quiet -stats -re -i pipe: -g 1 -c:v copy -c:a aac -f flv rtmp://127.0.0.1:1935/live/f1tv
     #else
-	#  GLOBALNAME=$(curl -s --request GET --url https://f1tv.formula1.com/2.0/R/DEU/BIG_SCREEN_HLS/ALL/PAGE/395/F1_TV_Pro_Annual/2 | jq -r '[.resultObj.containers[].retrieveItems.resultObj.containers[].metadata | select(.contentType == "VIDEO")][0].emfAttributes.Global_Title')
-	#  streamlink $URL best --hls-audio-select="de" --record-and-pipe "/record/$GLOBALNAME.mp4" | ffmpeg -re -i pipe: -c:v copy -c:a aac -f flv rtmp://127.0.0.1:1935/live/f1tv
-	#fi
+	    #GLOBALNAME=$(curl -s --request GET --url https://f1tv.formula1.com/2.0/R/DEU/BIG_SCREEN_HLS/ALL/PAGE/395/F1_TV_Pro_Annual/2 | jq -r '[.resultObj.containers[].retrieveItems.resultObj.containers[].metadata | select(.contentType == "VIDEO")][0].emfAttributes.Global_Title')
+	    #streamlink $URL best --hls-audio-select="de" --hls-duration 02:30:00 --record-and-pipe "/record/$GLOBALNAME.mp4" | ffmpeg -hide_banner -loglevel quiet -stats -re -i pipe: -c:v copy -c:a aac -f flv rtmp://127.0.0.1:1935/live/f1tv
+	  #fi
   fi
   
   #Check status of live session every 5 minutes to avoid ip temp ban
