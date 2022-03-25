6c4d369c60c658302aa996c1c9e48e14b4b382a5
SeniorHeld <67925751+SeniorHeld@users.noreply.github.com>
2022-03-25 23:24:37 +0100

    Added idle "functionality"
    When no stream is running on F1TV an "idle video" is transmitted

--- app/f1tv.sh
+++ app/f1tv.sh
@@ -23,7 +23,7 @@ if [[ -z $OUTPUT && $RECORD == true ]]; then
 elif [[ -z $OUTPUT && $RECORD == false ]]; then
   OUTPUT='rtmp://127.0.0.1:1935/live/f1tv'
 fi
-sleep 5
+
 DATA=$(echo '{"Login": "'$F1TV_EMAIL'", "Password": "'$F1TV_PASSWORD'"}')
 MAP="0:m:language:$LANGUAGE?"
 
@@ -47,12 +47,12 @@ while $true; do
     ContentId=$(curl -s --request GET --url https://f1tv.formula1.com/2.0/R/DEU/BIG_SCREEN_HLS/ALL/PAGE/395/F1_TV_Pro_Annual/2 | jq -r '[.resultObj.containers[].retrieveItems.resultObj.containers[].metadata | select(.contentType == "VIDEO") | select((.titleBrief == "Rennen") or (.titleBrief == "Qualifying") or (.titleBrief | startswith("Training")))][0].contentId')
   fi
 
-  #Get Stream URL
-  URL=$(curl -s --request GET --url "https://f1tv.formula1.com/1.0/R/DEU/BIG_SCREEN_HLS/ALL/CONTENT/PLAY?contentId=$ContentId" --header "ascendontoken: $AccessToken" | jq -r '.resultObj.url')
-  if [ "$URL" = "null" ]
+  if [ "$ContentId" = "null" ]
   then
     echo "ERROR: URL is null. Maybe there is no live stream?"
   else
+    #Get Stream URL
+    URL=$(curl -s --request GET --url "https://f1tv.formula1.com/1.0/R/DEU/BIG_SCREEN_HLS/ALL/CONTENT/PLAY?contentId=$ContentId" --header "ascendontoken: $AccessToken" | jq -r '.resultObj.url')
     if [ $RECORD = "true" ]; then
       GLOBALNAME=$(curl -s --request GET --url https://f1tv.formula1.com/2.0/R/DEU/BIG_SCREEN_HLS/ALL/PAGE/395/F1_TV_Pro_Annual/2 | jq -r '[.resultObj.containers[].retrieveItems.resultObj.containers[].metadata | select(.contentType == "VIDEO")][0].emfAttributes.Global_Title')
       echo GLOBALNAME: "$GLOBALNAME"
@@ -62,26 +62,31 @@ while $true; do
       echo '#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:BANDWIDTH=415000,RESOLUTION=480x270
-f1tv_mini.m3u8
+streams/f1tv_mini.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=677000,RESOLUTION=512x288
-f1tv_low.m3u8
+streams/f1tv_low.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=1200000,RESOLUTION=640x360
-f1tv_mid.m3u8
+streams/f1tv_mid.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=1993000,RESOLUTION=960x540
-f1tv_high.m3u8
+streams/f1tv_high.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=3730000,RESOLUTION=1280x720
-f1tv_hd.m3u8
+streams/f1tv_hd.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=6286000,RESOLUTION=1920x1080
-f1tv_fhd.m3u8
+streams/f1tv_fhd.m3u8
 ' > /srv/www/f1tv.m3u8
       ffmpeg -y -hide_banner -loglevel warning -re -i "$URL" \
 -map 0:p:5:v -map "$MAP" -c:v copy -c:a aac -f flv $OUTPUT \
--map 0:p:5:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 4 -hls_playlist_type event -hls_list_size 10 /srv/www/f1tv_fhd.m3u8 \
--map 0:p:4:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 4 -hls_playlist_type event -hls_list_size 10 /srv/www/f1tv_hd.m3u8 \
--map 0:p:3:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 4 -hls_playlist_type event -hls_list_size 10 /srv/www/f1tv_high.m3u8 \
--map 0:p:0:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 4 -hls_playlist_type event -hls_list_size 10 /srv/www/f1tv_mid.m3u8 \
--map 0:p:2:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 4 -hls_playlist_type event -hls_list_size 10 /srv/www/f1tv_low.m3u8 \
--map 0:p:1:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 4 -hls_playlist_type event -hls_list_size 10 /srv/www/f1tv_mini.m3u8
+-map 0:p:5:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 5 -hls_list_size 50 -hls_flags delete_segments /srv/www/streams/f1tv_fhd.m3u8 \
+-map 0:p:4:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 5 -hls_list_size 50 -hls_flags delete_segments /srv/www/streams/f1tv_hd.m3u8 \
+-map 0:p:3:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 5 -hls_list_size 50 -hls_flags delete_segments /srv/www/streams/f1tv_high.m3u8 \
+-map 0:p:0:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 5 -hls_list_size 50 -hls_flags delete_segments /srv/www/streams/f1tv_mid.m3u8 \
+-map 0:p:2:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 5 -hls_list_size 50 -hls_flags delete_segments /srv/www/streams/f1tv_low.m3u8 \
+-map 0:p:1:v -map "$MAP" -c:v copy -c:a aac -f hls -hls_time 5 -hls_list_size 50 -hls_flags delete_segments /srv/www/streams/f1tv_mini.m3u8
+echo '#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=415000,RESOLUTION=480x270
+streams/idle.m3u8
+' > /srv/www/f1tv.m3u8
     else
       echo "error"
     fi
